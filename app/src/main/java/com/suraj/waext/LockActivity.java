610ac3bbba3cfb5a30b3ec5794a7dd8846a38559
Jasi <Jaspreetsinghplayer@gmail.com>
2017-10-21 12:02:51 +0530

    not required
    not required

--- app/src/main/java/com/suraj/waext/LockActivity.java
+++ app/src/main/java/com/suraj/waext/LockActivity.java
@@ -142,14 +142,5 @@ public class LockActivity extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
     }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        File prefsDir = new File(this.getApplicationInfo().dataDir, "shared_prefs");
-        File prefsFile = new File(prefsDir, Utils.MYPREFS + ".xml");
-        if (prefsFile.exists()) {
-            prefsFile.setReadable(true, false);
-        }
-    }
+    
 }
